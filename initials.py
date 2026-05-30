0708d08206e3b24e09d63cb5f18e470c8a814da7
SereneKlenam-hub <2425400997@live.gctu.edu.gh>
2026-05-30 21:25:42 +0200

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
-
+# File: initials.py
+# Description: Print out my initials in stylized large block letters(SKM).
+# Assignment Number: 2
+#
+# Name: Serene Klenam Mensah
+# STUDENT ID: 2425400997
+# Email: 2425400997@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Serene Klenam Mensah, this programming assignment is my own work
+# and I have not provided this code to any other student.
+ 
+ 
 def main():
-  pass #code goes here
-
-
-
+   
+    print()
+    print("...SKM")
+    print()
+    print("....SSSSSSSSSS........KK........KK.......MM........MM.......")
+    print("...SS.................KK.......KK........MMM......MMM.......")
+    print("...SS.................KK......KK.........MMMM....MMMM.......")
+    print("...SS.................KK.....KK..........MM.MM..MM.MM.......")
+    print("....SSSSSSSSSS........KKKKKKKK...........MM..MMMM..MM.......")
+    print(".............SS.......KKKKKKKK...........MM..MMMM..MM.......")
+    print(".............SS.......KK.....KK..........MM........MM.......")
+    print(".............SS.......KK......KK.........MM........MM.......")
+    print(".............SS****...KK.......KK.****...MM........MM****...")
+    print("....SSSSSSSSSS.****...KK........KK****...MM........MM****...")
+    print()
+ 
+ 
 main()
